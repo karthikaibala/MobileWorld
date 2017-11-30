ddb06be27e7e70009629fe084871a9352bdb0df1
karthikai bala <ganakarthika24@gmail.com>
2017-11-30 16:06:13 +0530

    My project setup is compeleted successfully

---
+++ MobileWorldBackend/src/main/java/com/karthika/MobileWorldBackend/App.java
@@ -0,0 +1,13 @@
+package com.karthika.MobileWorldBackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
